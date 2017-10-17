acbda0dc8c5168217e870bc24c4803627394d19c
yusei-sako-z <yuu9534@zigexn.co.jp>
2017-10-17 16:01:55 +0900

    インデント修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,13 +7,15 @@ class ApplicationController < ActionController::Base
   end
 
   private
+  
   def sign_in_required
     return redirect_to new_user_session_url unless user_signed_in?
   end
 
   protected
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:username])
-    end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
