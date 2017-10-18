057c00891a5028de775f5d82ff97b5c3ac192fd5
yusei-sako-z <yuu9534@zigexn.co.jp>
2017-10-18 18:39:31 +0900

    Revert "カラムの説明とlimitの追記"
    This reverts commit c479237acf422757570fb48c923447c06331cdda.

--- db/migrate/20171012063540_add_columns_to_users.rb
+++ db/migrate/20171012063540_add_columns_to_users.rb
@@ -1,5 +1,5 @@
 class AddColumnsToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :username, :string, limit: 20 # userの名前
+    add_column :users, :username, :string, limit: 20
   end
 end
