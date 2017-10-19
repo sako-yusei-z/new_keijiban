fe167f2ae10cccce820113513826f212b7bb6b33
yusei-sako-z <yuu9534@zigexn.co.jp>
2017-10-19 11:08:14 +0900

    テーブル、カラムの説明追記

--- db/migrate/20171012065129_create_posts.rb
+++ db/migrate/20171012065129_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.1]
   def change
-    create_table :posts do |t|
-      t.string :title, limit: 60
-      t.text :body, limit: 600
+    create_table :posts, comment: 'スレッド' do |t|
+      t.string :title, limit: 60, comment: 'スレッドタイトル'
+      t.text :body, limit: 600, comment: 'スレッド内容'
       t.references :user, foreign_key: true
 
       t.timestamps
