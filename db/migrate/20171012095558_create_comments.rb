91abd98e61e717c6c7ff640f898a63aa67456c88
yusei-sako-z <yuu9534@zigexn.co.jp>
2017-10-19 11:08:50 +0900

    テーブル、カラムの説明追記

--- db/migrate/20171012095558_create_comments.rb
+++ db/migrate/20171012095558_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[5.1]
   def change
-    create_table :comments do |t|
-      t.string :body, limit: 60
+    create_table :comments, comment: 'スレッドのコメント' do |t|
+      t.string :body, limit: 60, comment: 'コメント内容'
       t.references :post, foreign_key: true
       t.references :user, foreign_key: true
 
