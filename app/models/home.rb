b37d78a12d8c4fed8e48c5693cc42b0a602be303
yusei-sako-z <yuu9534@zigexn.co.jp>
2017-10-20 16:55:05 +0900

    使用しないので削除

--- app/models/home.rb
+++
@@ -1,9 +0,0 @@
-class Home < ApplicationRecord
-  def self.search(search) #self.でクラスメソッドとしている
-    if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
-      Post.where(['title LIKE ?', "%#{search}%"])
-    else
-      Post.all #全て表示。
-    end
-  end
-end
