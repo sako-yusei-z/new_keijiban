0aee620ebb793497dd71465c0439236729e61e84
yusei-sako-z <yuu9534@zigexn.co.jp>
2017-10-20 14:46:39 +0900

    searchメソッドの追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,17 @@ class Post < ApplicationRecord
 
   validates :title, presence: true, length: { in: 1..30 }
   validates :body, presence: true, length: { in: 1..300 }
+
+  def self.search(search)
+    #if search
+    #  @posts = Comment.where(['body LIKE ?', "%#{search}%"])
+    #else
+    #  @posts = Comment.all
+    #end
+    if search
+      @posts = Post.where(['title LIKE ?', "%#{search}%"])
+    else
+      @posts = Post.all
+    end
+  end
 end
