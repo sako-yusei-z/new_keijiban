ec5d6935ac3b0e9225679c159031b0bc1af8237b
yusei-sako-z <yuu9534@zigexn.co.jp>
2017-10-20 18:41:39 +0900

    dependent: destroyの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :posts, dependent: :destroy
-  has_many :comments, dependent: :destroy
-  has_many :replies, dependent: :destroy
+  has_many :posts
+  has_many :comments
+  has_many :replies
 end
