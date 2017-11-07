4251bdd09fec64d1f881950e3a926ee4a8e2d59c
yusei-sako-z <yuu9534@zigexn.co.jp>
2017-11-07 10:52:11 +0900

    多対多の構築と,searchメソッドにタグ検索追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,20 +2,24 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :replies, dependent: :destroy
+  has_many :post_tags, dependent: :destroy
+  has_many :tags, through: :post_tags, dependent: :destroy
+  accepts_nested_attributes_for :tags,
+                                allow_destroy: true,
+                                reject_if: proc{ |attributes| attributes['category'].blank? }
 
   validates :title, presence: true, length: { in: 1..30 }
   validates :body, presence: true, length: { in: 1..300 }
 
-  def self.search(search)
-    #if search
-    #  @posts = Comment.where(['body LIKE ?', "%#{search}%"])
-    #else
-    #  @posts = Comment.all
-    #end
-    if search
-      @posts = Post.where(['title LIKE ?', "%#{search}%"])
-    else
+  def self.title_search(search)
+    @posts = Post.where(['title LIKE ?', "%#{search}%"])
+  end
+
+  def self.tag_search(search)
+    if search.blank?
       @posts = Post.all
+    else
+      @posts = Post.joins(:tags).where(["category LIKE ?", "%#{search}%"])
     end
   end
 end
