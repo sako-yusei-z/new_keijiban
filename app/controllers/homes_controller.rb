c909123b2122637e863c2df447e4e6cdd3da0132
yusei-sako-z <yuu9534@zigexn.co.jp>
2017-11-07 10:45:06 +0900

    routes変更に伴うuser情報の削除

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,10 +1,5 @@
 class HomesController < ApplicationController
   def index
     @posts = Post.all
-    if user_signed_in?
-      @user = current_user
-    else
-      @user = User.find_by(params[:user_id])
-    end
   end
 end
