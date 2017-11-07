a877dc10876c8e489792755f15f8123056ddb228
yusei-sako-z <yuu9534@zigexn.co.jp>
2017-11-07 10:48:49 +0900

    routes変更に伴うuser情報削除

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,6 +1,5 @@
 class RepliesController < ApplicationController
   def create
-    @user = User.find(params[:user_id])
     @post = Post.find(params[:post_id])
     @comment = Comment.find(params[:comment_id])
     @comment.replies.new(reply_params)
@@ -9,7 +8,7 @@ class RepliesController < ApplicationController
     @post.touch
     @post.save
 
-    return redirect_to user_post_path(@user, @post)
+    return redirect_to post_path(@post)
   end
 
   private
