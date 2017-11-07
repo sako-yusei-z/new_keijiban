0e59e44db5de7e8c9887adc4503e0f74ec2048a7
yusei-sako-z <yuu9534@zigexn.co.jp>
2017-11-07 10:40:29 +0900

    routes変更に伴いuser情報削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,15 @@
 class CommentsController < ApplicationController
-
   def create
-    @user = current_user
     @post = Post.find(params[:post_id])
     @post.comments.new(comment_params)
     @post.touch
     @post.save
 
-    return redirect_to user_post_path(@user, @post)
+    return redirect_to post_path(@post)
   end
 
   private
-  
+
   def comment_params
     params.require(:comment).permit(:body, :user_id)
   end
