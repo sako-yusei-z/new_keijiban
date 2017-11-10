0680d178415c1905772332ff321b7235b2390167
yusei-sako-z <yuu9534@zigexn.co.jp>
2017-11-10 18:53:34 +0900

    categoryカラムが重複しないようにunique制約追記

--- db/migrate/20171025060554_create_tags.rb
+++ db/migrate/20171025060554_create_tags.rb
@@ -4,6 +4,8 @@ class CreateTags < ActiveRecord::Migration[5.1]
       t.string :category, null: true, limit: 100, comment: 'タグ'
 
       t.timestamps
+
+      t.index ["category"], unique: true
     end
   end
 end
